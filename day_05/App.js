af14a64637c9cb72fa9125343673e26e543f6952
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-09-23 16:09:28 +0200

    modify app with the drawing number and with the various method of giving data to component

--- day_05/App.js
+++ day_05/App.js
@@ -11,60 +11,69 @@ class App extends React.Component{
     // }
     
     state = {
-        text: ""
+        text: "",
+        btn: "uruchom"
     }
 
     //funkcja strzałkowa
     handleClick = () => {
         //this.state.text += "a"; ale nie zadziala bo nie wywoła ponownie funkcji render() i nie uaktualni strony
-        const letter = "a";
+        const number = Math.floor(Math.random()*10);
         this.setState({
-            text: this.state.text + letter
+            text: this.state.text + number
         })
     }
 
     //metoda deklarowana
     
     render(){
+        const btnName = "stwórz liczbę";
         return (
             <React.Fragment>
-                <button onClick={this.handleClick}>Dodaj "A"</button>
-                
-                <h1>{this.state.text}</h1>
+                {/* <button onClick={this.handleClick}>{btnName}</button> */}
+                {/* <button onClick={this.handleClick}>{this.state.btn}</button> */}
+                <button onClick={this.handleClick}>{this.props.btnTitle}</button>
+                <PanelResult data={this.state.text}>bbb</PanelResult>
             </React.Fragment>
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const PanelResult = (props) => {
+    return (
+        <h1>{props.data}:{props.children}</h1>
+    )
+}
+ReactDOM.render(<App btnTitle="Dodaj cyfrkę"/>, document.getElementById('root'));
 
 //Funkcja strzałkowa a deklarowanie metody - bind
-handleClick() {
-    //this.state.text += "a"; ale nie zadziala bo nie wywoła ponownie funkcji render() i nie uaktualni strony
-    const letter = "a";
-    this.setState({
-        //tutaj this jest undefined, inaczej niż w funkcji strzałkowej
-        text: this.state.text + letter
-    })
-}
+// handleClick() {
+//     //this.state.text += "a"; ale nie zadziala bo nie wywoła ponownie funkcji render() i nie uaktualni strony
+//     const letter = "a";
+//     this.setState({
+//         //tutaj this jest undefined, inaczej niż w funkcji strzałkowej
+//         text: this.state.text + letter
+//     })
+// }
 
-<button onClick={this.handleClick.bind(this)}>Dodaj "A"</button> 
+{/* <button onClick={this.handleClick.bind(this)}>Dodaj "A"</button>  */}
 //i wtedy można metodę deklarowaną
 
 //setState - przekazanie do metody obiektu i funkcji
-this.setState((prevState)=>{
-    return{
-        text: this.prevState.text+letter
-    }
-})
+// this.setState((prevState)=>{
+//     return{
+//         text: this.prevState.text+letter
+//     }
+// })
 
 //Definiowanie state w konstruktorze (poprzedni sposob robienie, teraz można po prostu state = {} <- patrz wyżej)
-constructor(){
-    super(props);
-    this.state = {
-        text:""
-    }
+// constructor(){
+//     super(props);
+//     this.state = {
+//         text:""
+//     }
     
-    //inny sposob wiązania z bind zamiast funkcji strzałkowej
-    this.handleClick =this.handleClick.bind(this)
-}
+//     //inny sposob wiązania z bind zamiast funkcji strzałkowej
+//     this.handleClick =this.handleClick.bind(this)
+// }
+
