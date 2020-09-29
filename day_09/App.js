d6a5600f09dc723ed7313b9f4e79863ba1354cdf
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-09-29 13:46:30 +0200

    present using differend content in props depending on the condition, destructuring and function out of component but using component's state values

--- day_09/App.js
+++ day_09/App.js
@@ -1,6 +1,26 @@
-const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy!</p>;
+const displayMessage=(isConfirmed, isFormSubmitted)=>{
+        if(isFormSubmitted){
+            
+            if(isConfirmed ){
+                return <ValidationMessage txt="Możesz obejrzeć film. Zapraszamy!"/>
+            }
+            else{
+                return <ValidationMessage txt="Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!"/>
+            }
+        }
+        
+    }
 
-const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!</p>;
+const ValidationMessage = (props) => {
+    const {txt} = props
+    // const {txt, title} = props
+    return(
+        <p>{txt}</p>
+    )
+}
+// const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy!</p>;
+
+// const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!</p>;
 
 class TicketShop extends React.Component{
     state ={
@@ -27,32 +47,41 @@ class TicketShop extends React.Component{
     }
     
 
-    displayMessage=()=>{
-        if(this.state.isFormSubmitted){
-            if(this.state.isConfirmed ){
-                return <PositiveMessage/>
-            }
-            else{
-                return <NegativeMessage/>
-            }
-        }
+    // displayMessage=()=>{
+    //     if(this.state.isFormSubmitted){
+            
+    //         if(this.state.isConfirmed ){
+    //             return <ValidationMessage txt="Możesz obejrzeć film. Zapraszamy!"/>
+    //         }
+    //         else{
+    //             return <ValidationMessage txt="Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!"/>
+    //         }
+    //     }
         
-    }
+    // }
     render(){
+
+        const {isConfirmed, isFormSubmitted} = this.state
+        
         return(
             <>
             <h1>Kup bilet na horror roku!</h1>
             <form onSubmit={this.handleFormSubmitted}>
-                <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
+                <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={isConfirmed}/>
                 <label htmlFor="age">Mam co najmniej 16 lat</label>
                 <br/>
                 <button type="submit" >Kup bilet</button>
             </form>
-            {/* {this.state.isConfirmed ? <PositiveMessage/> : <NegativeMessage/>} */}
-            {this.displayMessage()}
+            
+            {displayMessage(isConfirmed, isFormSubmitted)}
             </>
         )
     }
 }
 
 ReactDOM.render(<TicketShop/>, document.getElementById('root'))
+
+//Part 3: 
+// a) jeden komponent z dwoma różnymi tekstami
+// b) destrukturyzacja
+// c) funkcja poza komponentem
