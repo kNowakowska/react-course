ba5c7dbc3ec5030543f3c854942fe14b89aad8b6
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-09-30 14:38:39 +0200

    add styles to the span element depending on the value in state

--- day_10/App.js
+++ day_10/App.js
@@ -29,7 +29,7 @@ class App extends React.Component{
         return(
             <>
                 <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-                <span> {this.state.shoppingCart} </span>
+                <span style={this.state.shoppingCart==0 ? { opacity: 0.3} : {}}> {this.state.shoppingCart} </span>
                 <button disabled={this.state.shoppingCart==this.state.availableProducts ? true : false}onClick={this.handleAddToCart}>+</button>
                 {this.state.shoppingCart>0 && <button onClick={this.handleBuying}>Kup</button>}
             </>
