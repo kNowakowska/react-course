fe2f84ff8ce51fa0f62a24de59c517c46b472f3e
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-09-30 14:34:09 +0200

    add 'buy' button and change state of cart and available products after buying

--- day_10/App.js
+++ day_10/App.js
@@ -1,3 +1,7 @@
+//Warunkowe dodanie przycisku i obsługa stanu magazynu
+// Wyszarzenie liczby 0 - css
+// destrukturyzacja
+
 class App extends React.Component{
     state = {
         availableProducts: 7,
@@ -15,12 +19,19 @@ class App extends React.Component{
             shoppingCart: this.state.shoppingCart+1
         })
     }
+    handleBuying = () =>{
+        this.setState({
+            availableProducts: this.state.availableProducts-this.state.shoppingCart,
+            shoppingCart: 0
+        })
+    }
     render(){
         return(
             <>
                 <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
                 <span> {this.state.shoppingCart} </span>
                 <button disabled={this.state.shoppingCart==this.state.availableProducts ? true : false}onClick={this.handleAddToCart}>+</button>
+                {this.state.shoppingCart>0 && <button onClick={this.handleBuying}>Kup</button>}
             </>
         )
     }
