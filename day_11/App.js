b595e5ba828b484e1a551eadbd0759f35c8bcfaf
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-10-01 14:37:17 +0200

    simple app showing how to work with lists, part 3: using data from external objects

--- day_11/App.js
+++ day_11/App.js
@@ -1,24 +1,52 @@
-const Item = (props) => {
+const data = {
+    users: [
+        {
+            id: 1,
+            age:29,
+            name: "Arek"
+        },
+        {
+            id: 2, 
+            age: 49,
+            name: "Marta"
+        },
+        {
+            id:3, 
+            age: 19,
+            name: "Stasia"
+        },
+        {
+            id:4,
+            age:24,
+            name:"Karol"
+        }
+    ]
+}
+
+const Item = ({user}) => {
     return(
-        <li>{`owoc ${props.item}`}</li>
+        <div>
+            <h1>Użytkownik {user.name}</h1>
+            <h2>Ma {user.age}</h2>
+        </div>
     )
 }
 
 class ListItems extends React.Component{
 
-    state = {
-        items: ["jabłko", "sliwka", "gruszka"]
-    }
+    // state = {
+    //     items: ["jabłko", "sliwka", "gruszka"]
+    // }
 
     render(){
-        const Items = this.state.items.map((item) => <Item item={item} key={item}/>)
+        const users = this.props.data.users;
+        const Items = users.map((user) => <Item user={user} key={user.id}/>)
         return(
             <ul>
-                {/* Użycie template stringa */}
                 {Items}
             </ul>
         )
     }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data}/>, document.getElementById('root'))
