ca5a85e95c1978e738ccd8f243d2fd5f5dff5c50
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-10-03 15:40:59 +0200

    simple app with currency exchange counter, part 2: using map method to create as many Cash component as there are in currencies table, also giving data through the props object

--- day_14/App.js
+++ day_14/App.js
@@ -1,12 +1,3 @@
-// const Dollars = (props) => (
-//     <div>Wartość w dolarach: {props.amount<=0 ? "" : (props.amount /props.ratio).toFixed(2)}</div>
-// )
-    
-
-// const Euros = (props) => (
-//     <div>Wartość w euro: {props.amount<=0 ? "" : (props.amount/props.ratio).toFixed(2)}</div>
-// )
-
 const Cash = (props) => {
     return (
     <>
@@ -17,23 +8,46 @@ const Cash = (props) => {
 class ExchangeCounter extends React.Component{
     state = {
         amount: "",
-        ratioDollar: 3.6,
-        ratioEuro: 4.2
+        // ratioDollar: 3.6,
+        // ratioEuro: 4.2
     }
+
+    currencies = [
+        {
+            id: 1,
+            name: "dollar",
+            ratio: 3.6,
+            title: "Wartość w dolarach"
+        },
+        {
+            id: 2,
+            name: "euro",
+            ratio: 4.1,
+            title: "Wartość w euro"
+        },
+        {
+            id: 3,
+            name: "pound",
+            ratio: 4.55,
+            title: "Wartość w funtach"
+        }
+    ]
     handleChange = e => {
         this.setState({
             amount: e.target.value
         })
     }
     render() {
-        const {amount, ratioDollar, ratioEuro} = this.state;
+        const {amount} = this.state;
+        const calculators = this.currencies.map(currency => (<Cash key={currency.id} title={currency.title} ratio={currency.ratio} amount={amount}/>))
         return(
             <React.Fragment>
                 <label> <input type="number" value={amount} onChange={this.handleChange}/></label>
                 {/* <Dollars ratio={ratioDollar} amount={amount}/>
                 <Euros ratio={ratioEuro} amount={amount}/> */}
-                <Cash ratio={ratioDollar} title={"Wartość w $: "} amount={amount}/>
-                <Cash ratio={ratioEuro} title={"Wartość w Euro: "} amount={amount}/>
+                {/* <Cash ratio={ratioDollar} title={"Wartość w $: "} amount={amount}/>
+                <Cash ratio={ratioEuro} title={"Wartość w Euro: "} amount={amount}/> */}
+                {calculators}
             </React.Fragment>
         )
     }
