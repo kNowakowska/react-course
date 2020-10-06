3a7ffa62bc75f4668b5df85c41d4d4313ebae2ce
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-10-06 11:50:48 +0200

    second version of predictions app, which enable for user to add new predictions

--- day_15/App.js
+++ day_15/App.js
@@ -6,46 +6,69 @@
 // Wersja na 5 - zobacz wróżbę, losuje jedną z trzech wróżb
 // Wersja na 6 - dodaje do trzech istniejących wróżb kolejną
 
-const predictions = [
-    {
-        name: "pierwsza wróżba",
-        id: 1
-    },
-    {
-        name: "druga wróżba",
-        id: 2
-    },
-    {
-        name: "trzecia wróżba",
-        id: 3
-    }
-]
+
 
 class App extends React.Component{
     state={
-        prediction: ""
+        prediction: "",
+        newPrediction: ""
     }
 
+    predictions = [
+        {
+            name: "pierwsza wróżba",
+            id: 1
+        },
+        {
+            name: "druga wróżba",
+            id: 2
+        },
+        {
+            name: "trzecia wróżba",
+            id: 3
+        }
+    ]
+
     drawPrediction=()=>{
-        return (Math.floor(Math.random()*this.props.predictions.length )+1)
+        return (Math.floor(Math.random()*this.predictions.length )+1)
     }
 
     handleShowPrediction=()=>{
        let id = this.drawPrediction() 
-       let value = this.props.predictions.filter(item => item.id===id)
+       let value = this.predictions.filter(item => item.id===id)
        value = value[0].name
         this.setState({
             prediction: value
+        })
+    }
+
+    handleAddPrediction= e=>{
+        e.preventDefault();
+        let newPred = {name:this.state.newPrediction, id: this.predictions.length+1}
+        this.predictions.push(newPred)
+        this.setState({
+            newPrediction: ""
+        })
+       
+    }
+    handleNewPrediction = e => {
+        const content = e.target.value
+        this.setState({
+            prediction: "",
+            newPrediction: content
         })
     }
     render(){
         return(
             <>
                 <button onClick={this.handleShowPrediction}>zobacz wróżbę</button>
+                <form onSubmit={this.handleAddPrediction}>
+                    <input type="text" onChange={this.handleNewPrediction} value={this.state.newPrediction}/><button>Dodaj wróżbę</button>
+                </form>
                 <h1>{this.state.prediction}</h1>
             </>
         )
     }
 }
 
-ReactDOM.render(<App predictions={predictions} />, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
