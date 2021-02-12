257219db17d2a2437bfdb50e92894334ee711e18
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2021-02-12 13:40:03 +0100

    first part of simple spa project

---
+++ day_25/src/layouts/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <div>
+      spa
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
