f96770497d3f88d7103b5066bb7165c4930a12b1
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2021-02-22 18:56:26 +0100

    SPA project: create and style header

--- day_25/src/layouts/App.js
+++ day_25/src/layouts/App.js
@@ -1,5 +1,5 @@
 import '../styles/App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Header from './Header';
 import Navigation from './Navigation';
 import Page from './Page';
--- day_25/src/layouts/Header.js
+++ day_25/src/layouts/Header.js
@@ -1,6 +1,42 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import "../styles/Header.css";
+import header1 from "../img/header1.jpg";
+import header2 from "../img/header2.jpg";
+import header3 from "../img/header3.jpg";
+
+
 const Header = () => {
-    return ( <div>Header</div> );
+    // const images = [header1, header2, header3];
+    // const index = Math.floor(Math.random()*3);
+    // const img = images[index];
+    return ( 
+        <>
+        {/* Swicth sprawia ze wykonuje sie jeden  Route a nie wszystkie pasujace */}
+         <Switch>
+            <Route path='/' exact render={()=>(
+                <img src={header1} alt="miasto"/>
+            )}/>
+            <Route path='/products' render={()=>(
+                <img src={header1} alt="miasto"/>
+            )}/>
+            <Route path='/contact' render={()=>(
+                <img src={header2} alt="miasto"/>
+            )}/>
+            <Route path='/admin' render={()=>(
+                <img src={header3} alt="miasto"/>
+            )}/>    
+             {/* Route bez atrybutu path dla widoku o nieistniejącym url  */}
+            <Route  render={()=>(
+                <img src={header2} alt="miasto"/>
+            )}/>  
+        </Switch>
+         
+
+         {/* <img src={img} alt="header"/> */}
+        
+        </>
+     );
 }
  
 export default Header;
